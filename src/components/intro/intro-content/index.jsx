2732f551b2ba79be64e5002d50fddb2a744d026b
Tilio77 <tiliobolso1@gmail.com>
2022-11-27 15:33:32 -0300

    First Commit

---
+++ src/components/intro/intro-content/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import hand from '../../../images/hand.png';
+import CallToAction from '../../shared/CallToAction';
+import './style.scss';
+import girl from '../../../images/girl.png';
+import { BsAwardFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+const IntroContent = () => {
+	return (
+		<div className="intro-content">
+			<div className="layout">
+				<div className="left-col">
+					<h1 className="title">
+						<span className="small-text">
+							<span className="text">Hello</span>
+							<span className="icon">
+								<img src={hand} />
+							</span>
+							<span className="text">, I Am</span>
+						</span>
+						<span className="big-text">JS Dev Hindi</span>
+					</h1>
+					<p>
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+						sed do eiusmod tempor incididunt ut labore et dolore 
+						magna aliqua."
+					</p>
+					<CallToAction text="Contact me" />
+				</div>
+
+				<div className="right-col">
+					<img src={girl} alt="JS Dev Hindi" />
+
+					<div className="highlights horizontal">
+						<div className="icon">
+							<BsAwardFill />
+						</div>
+						<div className="text">Best Design Award</div>
+					</div>
+
+					<div className="highlights verticle">
+						<div className="icon">
+							<FaUser />
+						</div>
+						<div className="text">
+							<span>4k+</span>
+							Happy Customers
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default IntroContent
---
+++ src/components/intro/navigation-bar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.scss';
+import logo from '../../../images/logo.png';
+import CallToAction from '../../shared/CallToAction';
+
+const Navigation = () => {
+	return (
+		<div className="top-navigation-bar">
+			<div className="app-logo">
+				<img src={logo} alt="JS Dev" />
+			</div>
+			<div className="navigation">
+				<span className="navigation-item">Skills</span>
+				<span className="navigation-item">Portfolio</span>
+				<span className="navigation-item">Blogs & Article</span>
+				<CallToAction text="Contact me" />
+			</div>
+		</div>
+	)
+}
+
+export default Navigation
